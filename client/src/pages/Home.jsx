38c2fbca3912f8bc1759e000a5f1615c7feb2a73
Abhinav Sinha <abhinavsinha135@gmail.com>
2024-01-02 14:16:40 +0530

    completed home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,49 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import SwiperCore from "swiper";
 import "swiper/css/bundle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ListingItem from "../components/ListingItem";
 
 const Home = () => {
-  const [offerListings, setOfferListings] = useState([]);
-  const [saleListings, setSaleListings] = useState([]);
-  const [rentListings, setRentListings] = useState([]);
+  const [travelListings, setTravelListings] = useState([]);
+  const [placementListings, setPlacementListings] = useState([]);
+  const [fitnessListings, setFitnessListings] = useState([]);
   SwiperCore.use([Navigation]);
+  console.log(travelListings);
+
+  useEffect(() => {
+    const fetchTravelListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?travel=true&limit=10");
+        const data = await res.json();
+        setTravelListings(data);
+        fetchPlacementListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchPlacementListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?placement=true&limit=10");
+        const data = await res.json();
+        setPlacementListings(data);
+        fetchFitnessListings();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const fetchFitnessListings = async () => {
+      try {
+        const res = await fetch("/api/listing/get?fitness=true&limit=10");
+        const data = await res.json();
+        setFitnessListings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTravelListings();
+  }, []);
 
   return (
     <div>
@@ -28,9 +64,9 @@ const Home = () => {
 
       {/* swiper */}
       <Swiper navigation>
-        {offerListings &&
-          offerListings.length > 0 &&
-          offerListings.map((listing) => (
+        {travelListings &&
+          travelListings.length > 0 &&
+          travelListings.map((listing) => (
             <SwiperSlide key={listing._id}>
               <div
                 style={{
@@ -38,10 +74,57 @@ const Home = () => {
                   backgroundSize: "cover",
                 }}
                 className="h-[500px]"
+                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
       </Swiper>
+
+      {/* listing results for travel, fitness and placements */}
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
+        {travelListings && travelListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent Travelling Blogs
+              </h2>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {travelListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {placementListings && placementListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent blogs for placements
+              </h2>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {placementListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {fitnessListings && fitnessListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent blogs for fitness
+              </h2>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {fitnessListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
