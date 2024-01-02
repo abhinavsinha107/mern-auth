8897dcb065ac322c631fec2de0efabe0c2ef9910
Abhinav Sinha <abhinavsinha135@gmail.com>
2024-01-02 12:16:16 +0530

    added contact landlord functionality to listing page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,56 @@
-const Contact = () => {
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Contact = ({listing}) => {
+    const [blogOwner, setBlogOwner] = useState(null);
+    const [message, setMessage] = useState("");
+    const onChange = (e) => {
+      setMessage(e.target.value);
+    };
+
+    useEffect(() => {
+      const fetchBlogOwner = async () => {
+        try {
+          const res = await fetch(`/api/user/${listing.userRef}`);
+          const data = await res.json();
+          setBlogOwner(data);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchBlogOwner();
+    }, [listing.userRef]);
+
   return (
-    <div>Contact</div>
-  )
+    <>
+      {blogOwner && (
+        <div className="flex flex-col gap-2">
+          <p>
+            Contact <span className="font-semibold">{blogOwner.username}</span>{" "}
+            for{" "}
+            <span className="font-semibold">{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name="message"
+            id="message"
+            rows="2"
+            value={message}
+            onChange={onChange}
+            placeholder="Enter your message here..."
+            className="w-full border p-3 rounded-lg"
+          ></textarea>
+
+          <Link
+            to={`mailto:${blogOwner.email}?subject=Regarding ${listing.name}&body=${message}`}
+            className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
+          >
+            Send Message
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
-export default Contact
+
+export default Contact;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -10,16 +10,19 @@ import {
 } from "react-icons/fa";
 import { SiYourtraveldottv } from "react-icons/si";
 import { IoFitness } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import Contact from "../components/Contact";
 
 const Listing = () => {
   SwiperCore.use([Navigation]);
   const params = useParams();
-
+  const {currentUser} = useSelector(state => state.user);
 
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [contact, setContact] = useState(false);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -102,6 +105,15 @@ const Listing = () => {
                 {listing.travel ? "Travelling" : "Not under Travelling"}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact Blog Owner
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
